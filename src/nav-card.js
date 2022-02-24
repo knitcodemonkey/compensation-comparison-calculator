410c1d195c7cba50e079a769d9abe709cff38c4c
Jen Luker <knittingcodemonkey@gmail.com>
2022-02-24 10:02:21 -0700

    Updating nav card

--- src/nav-card.js
+++ src/nav-card.js
@@ -1,68 +1,114 @@
-import { useRef, useEffect } from "react";
+import { useEffect, useState } from "react";
 import navLevels from "./nav-levels";
 import "./styles.css";
 
-const filterArr = (arr, key) =>
+const filterUniqueObjValues = (arr, key) =>
   [...new Map(arr.map((item) => [item[key], item])).values()].map(
     (obj) => obj[key]
   );
 
 export const NavCard = () => {
+  const [selectedTrack, setSelectedTrack] = useState("A");
+  const [selectedLevel, setSelectedLevel] = useState(1);
+  const [selectedStep, setSelectedStep] = useState(1);
+
+  const [trackObj, setTrackObj] = useState([]);
+  const [levelObj, setLevelObj] = useState([]);
+  const [levels, setLevels] = useState([]);
+  const [steps, setSteps] = useState([]);
+
   // filter the track
-  const filteredTrack = filterArr(navLevels, "Track");
-  const trackRef = useRef();
-  console.log(filteredTrack, trackRef.current);
+  const filteredTrack = filterUniqueObjValues(navLevels, "Track");
 
   // Filter levels by track
-  const levels = filterArr(
-    navLevels.map((level) => level).filter((obj) => obj[trackRef.current]),
-    "Level"
+  const filteredTrackObjs = navLevels.filter(
+    (obj) => obj["Track"] === selectedTrack
   );
-  const levelRef = useRef();
-  console.log(levels);
 
-  // Filter steps by track and level
-  const step = useRef();
+  useEffect(() => {
+    setLevelObj(
+      filteredTrackObjs
+        .map((track) => track)
+        .filter((obj) => obj["Track"] === selectedTrack)
+    );
+  }, [selectedTrack, filteredTrackObjs]);
 
-  useEffect(() => {}, [trackRef, levelRef, stepRef]);
+  useEffect(() => {
+    setLevelObj(
+      filteredTrackObjs
+        .map((track) => track)
+        .filter((obj) => obj["Track"] === selectedTrack)
+    );
+
+    setLevels(filterUniqueObjValues(levelObj, "Level"));
+  }, [selectedTrack]);
+
+  // This part may need to be set as a state variable, and then updated?
+  // We didn't need to do that with filteredTrackObjs though
+
+  const filteredLevelObjs = filteredTrackObjs
+    // .map((level) => level)- I think commenting this out helped?
+    .filter((obj) => obj["Level"] === selectedLevel);
+
+  // Filter Steps by Level and Track
+  useEffect(() => {
+    setSteps(filterUniqueObjValues(levelObj, "Step"));
+  }, [levelObj, selectedLevel]);
+
+  console.log({
+    filteredTrack,
+    selectedTrack,
+    filteredTrackObjs,
+    levels,
+    selectedLevel,
+    filteredLevelObjs,
+    steps,
+    selectedStep
+  });
 
   return (
     <div className="nav-card">
       <form>
         <div>
           <label htmlFor="track">Track</label>
-          <select id="track" ref={trackRef}>
+          <select
+            id="track"
+            value={selectedTrack}
+            onChange={(e) => setSelectedTrack(e.target.value)}
+          >
             {filteredTrack?.map((track) => (
               <option value={track} key={track}>
-                {`${level["Employee Level"]} - ${level["Description"]}`}
+                {track}
               </option>
             ))}
           </select>
         </div>
 
         <div>
-          <label htmlFor="employeeLevel">Employee Level</label>
-          <select id="employeeLevel" ref={track}>
-            {navLevels?.map((level) => (
-              <option
-                value={level["Employee Level"]}
-                key={level["Employee Level"]}
-              >
-                {`${level["Employee Level"]} - ${level["Description"]}`}
+          <label htmlFor="level">Level</label>
+          <select
+            id="level"
+            value={selectedLevel}
+            onChange={(e) => setSelectedLevel(e.target.value)}
+          >
+            {levels?.map((level) => (
+              <option value={level} key={level}>
+                {level}
               </option>
             ))}
           </select>
         </div>
 
         <div>
-          <label htmlFor="employeeLevel">Employee Level</label>
-          <select id="employeeLevel" ref={track}>
-            {navLevels?.map((level) => (
-              <option
-                value={level["Employee Level"]}
-                key={level["Employee Level"]}
-              >
-                {`${level["Employee Level"]} - ${level["Description"]}`}
+          <label htmlFor="step">Step</label>
+          <select
+            id="step"
+            value={selectedStep}
+            onChange={(e) => setSelectedStep(e.target.value)}
+          >
+            {steps?.map((step) => (
+              <option value={step} key={step}>
+                {step}
               </option>
             ))}
           </select>
